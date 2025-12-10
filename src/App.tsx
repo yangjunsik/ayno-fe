fec629950d3064364c50f449ad338a1a5c17017e
yangjunsig <wnstlr1379@naver.com>
2025-12-10 19:23:09 +0900

    feature: Implement dynamic SEO management using `react-helmet-async` and a dedicated `SEO` component.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import styled from '@emotion/styled';
 import { Outlet } from 'react-router-dom';
 import Header from './components/common/Header';
 import Footer from './components/common/Footer';
+import SEO from './components/common/SEO';
 
 const StyledMain = styled.main`
   min-height: calc(100vh - 300px); /* Prevent footer from jumping up */
@@ -10,6 +11,7 @@ const StyledMain = styled.main`
 function App() {
   return (
     <div>
+      <SEO />
       <Header />
       <StyledMain>
         {/* 2. [핵심] Outlet: 여기가 바로 URL에 따라 내용이 갈아 끼워지는 '구멍'입니다. */}
