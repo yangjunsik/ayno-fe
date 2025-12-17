8992d61c9d5b35870751c2f010d2f54ddcf87564
yangjunsig <wnstlr1379@naver.com>
2025-12-17 17:44:59 +0900

    remove unnecessary React import from AdminUserPage.

--- src/pages/admin/manage/AdminUserPage.tsx
+++ src/pages/admin/manage/AdminUserPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import styled from '@emotion/styled';
 import { getUsers, updateUserStatus } from '../../../api/adminUser';
 import type { AdminUserView, UserStatus } from '../../../types/adminUser';
